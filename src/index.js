4aa07cf8fb1cafd9b658f35445bdc5047169472f
François Beuvens <francois.beuvens@gmail.com>
2014-09-29 15:58:35 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ module.exports = function(mongoose){
 
 	if(!mongoose.Document.prototype.develop){
 		require('./Document')(mongoose);
-		//require('./Embedded')(mongoose);
+		require('./Embedded')(mongoose);
 		require('./Array')(mongoose);
 		require('./DocumentArray')(mongoose);
 	}
